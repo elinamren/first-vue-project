b69c87574118c4d9c19d50ee6ccc6b4f7f712aae
Elin Amren <elin.amren@gmail.com>
2021-09-14 15:46:32 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:elinamren/first-vue-project.git main:gh-pages
+git push -f git@github.com:elinamren/first-vue-project.git master:gh-pages
 cd -
 
