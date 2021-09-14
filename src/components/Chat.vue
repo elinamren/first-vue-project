5ca0b09db7da4f01c11d32f8c2430311b03e6a4f
Elin Amren <elin.amren@gmail.com>
2021-09-14 13:41:52 +0200

    chat answer

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -7,51 +7,115 @@
           alt="Bot"
         />
         <div class="bubble bot-bubble">
-          <p>Hej!</p>
+          <p>Hej! Vad heter du?</p>
         </div>
       </section>
     </section>
 
     <div class="input-wrapper" id="input-wrapper">
-      <form id="name-form">
+      <form id="name-form" v-if="showForm">
         <input id="name-input" type="text" ref="input" />
-        <button class="send-btn" type="submit" @click="showMessage">
+        <button class="send-btn" type="submit" @click="getNameAnswer">
           Send
         </button>
       </form>
+      <div class="button-wrapper" v-if="showButtons">
+        <button class="send-btn" type="button" @click="pepp">
+          Pepp
+        </button>
+        <button class="send-btn" type="button" @click="tarot">
+          Tarot kort
+        </button>
+        <button class="send-btn" type="button" @click="weather">
+          Veta vädret
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      name: "",
+      showButtons: false,
+      showForm: true,
+      peppQuotes: [
+        "Allt jag behöver finns inom mig.",
+        "Energin du sprider ut kommer komma tillbaka till dig.",
+        "Lita på att saker händer av en anledning",
+        "DU kan göra det!",
+        "Dröm stort.",
+        "Äg din lycka, fokusera på det som gör dig glad",
+      ],
+      tarotkort: [
+        "Månen, representerar falska beskyllningar, dolda fiender, och fara. Kortet kan i mer extrama tolkningar betyda mörker and fasa.",
+        "Rättvisan, står för rättvisans triumf, lag, sanning, kontroll och kontrakt. Å andra sidan kan också kortet dyka upp då man har problem med rättvisan och när man anklagar eller blir anklagad. Vissa tarottolkare anser kortet har med beslut att göra.",
+        "Vagnen, står för krig, vedergällning, hämnd och motstånd men även för skydd och triumf. Du kommer snart att ta kontrollen över situationen och gå vidare.",
+        "Två i bägare, står för kärlek. Du känner stark gemenskap med en partner, vän eller kollega. Eller har det skett ett oväntat möte med någon som du finner attraktiv eller som du tycker om? Två i bägare brukar även stå för förtroliga samtal mellan två människor.",
+        "Drottning i pentagram, en praktisk kvinna som man kan lita på. Hon är en riktig affärskvinna, smart och som kommer att kämpa för sin rätt. När det handlar om pengar kan den här kvinnan vara rätt så hård och envis. Som situation betyder kortet att man tänker praktiskt i materiella frågor. Drottning i Pentagram är snäll och delar med sig, men hon får inte alltid någon stöd från omgivningen, även när hon verkligen förtjänar det. Drottning i Pentagram har således en hel del gemensamt med Kejsarinnan och står även för alla kvinnliga healers. För en man kan kortet betyda att han är gift.",
+      ],
+    };
+  },
   methods: {
-    showMessage(event) {
-      event.preventDefault();
-      this.$refs.chat.innerHTML += `
-        <section class="user-msg">
-    <div class="bubble user-bubble">
-      <p>${this.$refs.input.value}</p>
-    </div>
-  </section>
-      `;
-      this.$refs.input.value = "";
-      this.getBotMessage("Hur mår du idag?");
+    getRandom(number) {
+      return Math.floor(Math.random() * number);
     },
-    getBotMessage(message) {
+    getNameAnswer(event) {
+      event.preventDefault();
+      this.showMessage(this.$refs.input.value, "user");
+      this.name = this.$refs.input.value;
+      this.$refs.input.value = "";
+      this.showMessage(`Trevligt ${this.name}, Vad behöver du idag?`, "bot");
+      this.showForm = false;
+      this.showButtons = true;
+    },
+    showMessage(message, sender) {
       setTimeout(() => {
-        this.$refs.chat.innerHTML += `
-              <section class="bot-msg">
-                  <img
-                      src="https://image.flaticon.com/icons/png/512/1587/1587565.png"
-                      alt="Bot"
-                  />
-                  <div class="bubble bot-bubble">
-                      <p>${message}</p>
-                  </div>
-              </section>
-            `;
+        const chat = this.$refs.chat;
+        if (sender === "user") {
+          chat.innerHTML += `
+            <section class="user-msg">
+                <div class="bubble user-bubble">
+                    <p>${message}</p>
+                </div>
+            </section>
+          `;
+        } else {
+          chat.innerHTML += `
+            <section class="bot-msg">
+                <img
+                    src="https://image.flaticon.com/icons/png/512/1587/1587565.png"
+                    alt="Bot"
+                />
+                <div class="bubble bot-bubble">
+                    <p>${message}</p>
+                </div>
+            </section>
+        `;
+        }
+        chat.scrollTop = chat.scrollHeight;
       }, 1000);
+    },
+
+    pepp() {
+      this.showMessage("Pepp", "user");
+      this.showMessage(
+        this.peppQuotes[this.getRandom(this.peppQuotes.length)],
+        "bot"
+      );
+    },
+    tarot() {
+      this.showMessage("Tarotkort", "user");
+      this.showMessage(
+        this.tarotkort[this.getRandom(this.tarotkort.length)],
+        "bot"
+      );
+    },
+    weather() {
+      this.showMessage("Vädret just nu", "user");
+      this.showMessage(`Sol och 10 grader`, "bot");
     },
   },
 };
@@ -119,6 +183,7 @@ input {
   padding: 16px 24px;
   color: #015081;
   max-width: 40%;
+  text-align: left;
 }
 
 .bot-bubble {
